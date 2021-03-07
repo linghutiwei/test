d88a711caee2f7fff5227f5c610623e6019ca230
Administrator <fanli9520.@163.com>
2021-03-07 20:41:43 +0800

    11111

--- kill/src/main/java/heihei.java
+++ kill/src/main/java/heihei.java
@@ -1,6 +1,11 @@
 public class heihei {
     public static void main(String[] args) {
         System.out.println(12);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
         System.exit(1);
     }
 }
