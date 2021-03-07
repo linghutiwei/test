41f5a209c26e394c4290f5972b1862d1da85ca87
Administrator <fanli9520.@163.com>
2021-03-07 18:42:27 +0800

    linghu

--- kill/src/main/java/heihei.java
+++ kill/src/main/java/heihei.java
@@ -2,5 +2,8 @@ public class heihei {
     public static void main(String[] args) {
         System.out.println(12);
         System.exit(1);
+        for (;;){
+            System.out.println("i love you");
+        }
     }
 }
